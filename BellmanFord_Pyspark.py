4e81b69e1e98bbe2dab0a7fc6b5f5bfd72c0652a
Greg Murray <gregmurray30@gmail.com>
2018-08-20 15:30:55 -0700

    Update BellmanFord_Pyspark.py

--- BellmanFord_Pyspark.py
+++ BellmanFord_Pyspark.py
@@ -5,10 +5,8 @@
 #sc = spark.sparkContext
 class SparkGraph:
     def __init__(self, rdd):
-            rdd2 = rdd.map(lambda x: (x[1][0], (x[0], -1*x[1][1])))
-            rdd3 = rdd.union(rdd2)
-            #self.graph_rdd = rdd3.repartition(1)
-            self.graph_rdd = rdd3
+            self.graph_rdd = rdd.repartition(1)
+            #self.graph_rdd = rdd
             self.nodes = []
             self.top_recs = []
             self.dist = {}
@@ -30,11 +28,13 @@ class SparkGraph:
                     return self.top_recs
             return self.top_recs[0:n]
     def excludeSourceSeen(self, src):
+        #excludes movies user has already seen and non-movie nodes
         src_seen = self.graph_rdd.filter(lambda x: x[0]==src).map(lambda x: x[1][0]).collect()
+        src_seen.append(src)
         src_seen = sc.broadcast(src_seen)
         dist_rdd = sc.parallelize(self.dist.items())
-        g.dist = dict(dist_rdd.filter(lambda x: x[0] not in src_seen.value).collect())
-        
+        g.dist = dict(dist_rdd.filter(lambda x: x[0] not in src_seen.value and x[0][1]=='m').collect())
+    
 
 
 def bellmanFord(g, src, n, degree_penalty=1):
@@ -42,34 +42,27 @@ def bellmanFord(g, src, n, degree_penalty=1):
     g.setDist(src)
     dgr_pen = sc.broadcast(degree_penalty)
     src_bc = sc.broadcast(src)
+    for i in range (1000):
+        print("")
     def compare_vs(x):
             d_v = dist.value.get(x[1][0], float("Inf"))
             d_u = dist.value.get(x[0], float("Inf"))
             w = x[1][1]
-            
-            if abs(d_u)<1:
-                if d_u<0 and x[0]!=src_bc.value:
-                    d_u-=1
-                elif x[0]!=src_bc.value:
-                    d_u+=1
-            if abs(d_v)<1:
-                if d_v<0 and x[1][0]!=src_bc.value:
-                    d_v-=1
-                elif x[1][0]!=src_bc.value:
-                    d_v+=1
-            #print(x[0], x[1][0], w)
-            #print('d_u:', d_u, 'd_v:', d_v)
-            if abs(dgr_pen.value*(w+d_u))<abs(d_v):
-                
-                if x[0]==src_bc.value:
-                    #print('returning d_u+w:', d_u+w)
+            if x[0]!=src_bc.value and x[1][0]!=src_bc.value:
+                w+=1
+            print(x[0], x[1][0], w)
+            print('d_u:', d_u, 'd_v:', d_v)
+            if dgr_pen.value*w+d_u<d_v:
+                #if u==source or u and v are in different network clusters
+                if x[0]==src_bc.value or x[0][1]!=x[1][0][1]:
+                    print('returning d_u+w:', d_u+w)
                     return (x[1][0] , w+d_u)
                 else:
                     #print('d_u', d_u)
-                    #print('returning d_u+dp**w:', dgr_pen.value*(w+d_u))
-                    return (x[1][0] , dgr_pen.value*(w+d_u))
+                    print('returning d_u+dp**w:', dgr_pen.value*(w+d_u))
+                    return (x[1][0] , dgr_pen.value*w+d_u)
             else:
-                #print('returning d_v:', d_v)
+                print('returning d_v:', d_v)
                 return (x[1][0], d_v)
     def abs_min(a, b):
         if abs(a)<abs(b):
@@ -80,20 +73,19 @@ def bellmanFord(g, src, n, degree_penalty=1):
             g.iters +=1
             dist = sc.broadcast(g.dist)
             rdd1 = g.graph_rdd.map(compare_vs)
+            print(rdd1.collect())
             t = time.time()
             min_i = rdd1.reduceByKey(abs_min).collect()
             g.times['min_i'] = g.times['min_i']+(time.time()-t)
             g.dist = dict(min_i)
-            #print('g.dist', g.dist)
+            print('g.dist', g.dist)
             if i==(len(g.nodes)/2):
                 g.snapshot = g.dist
-    # for k in g.dist:
-    #         g.dist[k] = abs(g.dist[k])
     g.excludeSourceSeen(src)
     return g.topRecommendations(n)
     
 
-# l = [('a', ('b', -3)), ('b', ('c', 1)), ('c', ('d', 1)), ('d', ('e', 1)), ('a', ('e', -3))]
+# l = [('1', ('34', 2.29)), ('34', ('3824', 3.0)), ('', ('d', 1)), ('d', ('e', 1)), ('a', ('e', -3))]
 # l2 = sc.parallelize(l)
 # g = SparkGraph(l2)
 # bellmanFord(g, 'a', 2, 1.1)
@@ -101,7 +93,9 @@ def bellmanFord(g, src, n, degree_penalty=1):
 
 # demonstrates the impact of the degree penalty parameter. Set relatively small 1.1, the shortest path to 'c'
 # utilizes more nodes a-e-d-c (remember negative the inverse pairs are negatively weighted) rather than
-# a-b-c. Turning the degree penalty up to 1.5 and the shortest path becomes
+#a-b-c. Turning the degree penalty up to 1.5 and the shortest path becomes
 
 g = SparkGraph(USER_MOVIE_NETWORK)
-bellmanFord(g,'1', 5, 1.1)
+bellmanFord(g,('1', 'u'), 5, 1.2)
+
+#g.graph_rdd.filter(lambda x: x[0]==('1', 'u') and x[1][0]==('2', 'u')).collect()
