e3e9b347166f489c6b480339e680b9dd60bfdbb3
Greg Murray <gregmurray30@gmail.com>
2018-09-21 16:47:51 -0700

    Update Spreading_Activation.py

--- Spreading_Activation.py
+++ Spreading_Activation.py
@@ -1,16 +1,16 @@
 
 def get_children(dist_arr, fired_nodes):
         arr_node_rdds = []
-        print('dist_arr', dist_arr)
+        #print('dist_arr', dist_arr)
         for i in range(len(dist_arr)):
             if dist_arr[i][0] in fired_nodes:
-                print('node', dist_arr[i][0], 'in node_list, continue\n ')
+                #print('node', dist_arr[i][0], 'in node_list, continue\n ')
                 continue
             node_id_bc = sc.broadcast(dist_arr[i][0])
             A = dist_arr[i][1]
             A_bc = sc.broadcast(A)
             temp_rdd_i = rdd_graph_shell.filter(lambda x: x[0]==node_id_bc.value)
-            print('node_id_bc:', node_id_bc.value, 'i:', i, 'temp_rdd_i:', temp_rdd_i.collect())
+            #print('node_id_bc:', node_id_bc.value, 'i:', i, 'temp_rdd_i:', temp_rdd_i.collect())
             temp_rdd_i = temp_rdd_i.mapValues(activate).collect()
             if temp_rdd_i!=[]:
                 arr_node_rdds.append(temp_rdd_i[0][1])
@@ -61,22 +61,22 @@ def walk_path(A, D, F, rdd_src, rdd_graph_shell, n=5):
        return TOP_N_RECOMMENDATIONS
 
 
+
 if __name__=="__main__":
-    #inv_MOVIE_NETWORK = MOVIE_NETWORK.map(lambda x: (x[1][0], (x[0], x[1][1])))
-    #MOVIE_NETWORK2 = MOVIE_NETWORK.union(inv_MOVIE_NETWORK) #have pairs A-B and B-A
-
-
-    MOVIE_NETWORK2=sc.parallelize([((u'122', 'm'), ((u'376', 'm'), 0.47)), ((u'122', 'm'), ((u'185', 'm'), 0.456)), ((u'231', 'm'), 
-    ((u'539', 'm'), 0.989)), ((u'185', 'm'), ((u'231', 'm'), 0.632)), ((u'231', 'm'), ((u'376', 'm'), 0.809)), ((u'122', 'm'), 
-    ((u'539', 'm'), 0)), ((u'376', 'm'), ((u'539', 'm'), 0.503)), ((u'122', 'm'), ((u'231', 'm'), 0.574)), ((u'185', 'm'), 
-    ((u'539', 'm'), 0.391)), ((u'185', 'm'), ((u'376', 'm'), 0.169)), ((u'376', 'm'), ((u'122', 'm'), 0.47)), ((u'185', 'm'), 
-    ((u'122', 'm'), 0.456)), ((u'539', 'm'), ((u'231', 'm'), 0.989)), ((u'231', 'm'), ((u'185', 'm'), 0.632)), ((u'376', 'm'), 
-    ((u'231', 'm'), 0.809)), ((u'539', 'm'), ((u'122', 'm'), 0)), ((u'539', 'm'), ((u'376', 'm'), 0.503)), 
-    ((u'231', 'm'), ((u'122', 'm'), 0.574)), ((u'539', 'm'), ((u'185', 'm'), 0.391)), ((u'376', 'm'), ((u'185', 'm'), 0.169))])
-    rdd_graph_shell = MOVIE_NETWORK2.combineByKey(li, app, ext)
-    rdd_src = rdd_graph_shell.filter(lambda x: x[0]==src)
+     #inv_MOVIE_NETWORK = MOVIE_NETWORK.map(lambda x: (x[1][0], (x[0], x[1][1])))
+     #MOVIE_NETWORK2 = MOVIE_NETWORK.union(inv_MOVIE_NETWORK) #have pairs A-B and B-A
+     src = ('122', 'm')
+     MOVIE_NETWORK2=sc.parallelize([((u'122', 'm'), ((u'376', 'm'), 0.47)), ((u'122', 'm'), ((u'185', 'm'), 0.456)), ((u'231', 'm'), 
+     ((u'539', 'm'), 0.989)), ((u'185', 'm'), ((u'231', 'm'), 0.632)), ((u'231', 'm'), ((u'376', 'm'), 0.809)), ((u'122', 'm'), 
+     ((u'539', 'm'), 0)), ((u'376', 'm'), ((u'539', 'm'), 0.503)), ((u'122', 'm'), ((u'231', 'm'), 0.574)), ((u'185', 'm'), 
+     ((u'539', 'm'), 0.391)), ((u'185', 'm'), ((u'376', 'm'), 0.169)), ((u'376', 'm'), ((u'122', 'm'), 0.47)), ((u'185', 'm'), 
+     ((u'122', 'm'), 0.456)), ((u'539', 'm'), ((u'231', 'm'), 0.989)), ((u'231', 'm'), ((u'185', 'm'), 0.632)), ((u'376', 'm'), 
+     ((u'231', 'm'), 0.809)), ((u'539', 'm'), ((u'122', 'm'), 0)), ((u'539', 'm'), ((u'376', 'm'), 0.503)), 
+     ((u'231', 'm'), ((u'122', 'm'), 0.574)), ((u'539', 'm'), ((u'185', 'm'), 0.391)), ((u'376', 'm'), ((u'185', 'm'), 0.169))])
+     rdd_graph_shell = MOVIE_NETWORK2.combineByKey(li, app, ext)
+     rdd_src = rdd_graph_shell.filter(lambda x: x[0]==src)
     
-    A=1 #Activation value for source
-    D=.5 #Decay factor
-    F=0 #Activation threshold
-    print(walk_path(A, D, F, rdd_src, rdd_graph_shell, 5))
+     A=1 #Activation value for source
+     D=.5 #Decay factor
+     F=0 #Activation threshold
+     recommendations = walk_path(A, D, F, rdd_src, rdd_graph_shell, 5)
